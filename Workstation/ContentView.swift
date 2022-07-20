e42efc0b36e8cee4c19e439dc7d42555c860343d
Adam Rafiandri <rafiandria23@gmail.com>
2022-07-21 03:04:24 +0700

    Initial Commit

---
+++ Workstation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workstation
+//
+//  Created by Adam Rafiandri on 07/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workstation/WorkstationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkstationApp.swift
+//  Workstation
+//
+//  Created by Adam Rafiandri on 07/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkstationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
